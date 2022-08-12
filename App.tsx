ca41651a211822229fbdf60bf762ccb82290c5dc
priscocleyton <priscocleyton@gmail.com>
2022-08-12 16:47:21 -0300

    removendo apploading depreciada e atualizando com a nova

--- App.tsx
+++ App.tsx
@@ -1,30 +1,65 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
+import React, { useCallback, useEffect, useState } from 'react';
+// import AppLoading from 'expo-app-loading';
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import { ThemeProvider } from 'styled-components/native';
 import { StatusBar } from 'expo-status-bar'
-
+import { View } from 'react-native';
 import theme from './src/theme';
 
 
 import { SignIn } from '@screens/SignIn';
 
-export default function App() {
-  
-  const [fontsLoaded] = useFonts({
-    DMSans_400Regular,
-    DMSerifDisplay_400Regular
-  });
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from "expo-font";
 
-  if (!fontsLoaded) {
-    return <AppLoading />
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          DMSans_400Regular,
+          DMSerifDisplay_400Regular
+        });
+        // await new Promise(resolve => setTimeout(resolve, 10000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar style="light" translucent backgroundColor="transparent"/>
-      <SignIn />
-    </ThemeProvider>
+    <View
+      onLayout={onLayoutRootView}
+      style={{
+        flex: 1
+      }}
+    >
+      <ThemeProvider theme={theme}>
+          <StatusBar style="light" translucent backgroundColor="transparent"/>
+          <SignIn />
+      </ThemeProvider>
+    </View>
   );
 }
